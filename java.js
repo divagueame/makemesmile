f8f21c28e38d57e143451fee71e2a2cd78059ff5
diva <martin.arce.teixeira@gmail.com>
2020-12-02 19:47:38 +0100

    Modal bug

--- java.js
+++ java.js
@@ -10,10 +10,10 @@
       var user = result.user;
       console.log("You have sucessfully signed through Google")
       console.log(token,user);
-      const modal = document.querySelector("#modal-signup");
+      // const modal = document.querySelector("#modal-signup");
       const modalLogin = document.querySelector("#modal-login");
 
-      M.Modal.getInstance(modal).close();
+      // M.Modal.getInstance(modal).close();
       M.Modal.getInstance(modalLogin).close();
     }).catch((err)=>{
       var errorMessage = err.message;
